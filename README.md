cd71d8c3a2d9458fd2a96eaea294c8cb3b618fc7
TAVii119 <tavigames@gmail.com>
2023-03-09 10:01:50 +0200

    Arm Subsystem with PID and Feedforward

--- README.md
+++
@@ -1,2 +0,0 @@
-# FRC-Charged-Up-2023
-Our repo for the 2023 FIRST Robotics Competition Season "Charged Up".
---
+++ nuconteaza/src/main/java/frc/robot/Constants/HardwareConstants.java
@@ -0,0 +1,9 @@
+package frc.robot.Constants;
+
+public static class Constants {
+    public static final int SWERVE_COAX_MOTOR_LF = 0;
+    public static final int SWERVE_COAX_MOTOR_LB = 0;
+    public static final int SWERVE_COAX_MOTOR_RF = 0;
+    public static final int SWERVE_COAX_MOTOR_RB = 0;
+
+}
